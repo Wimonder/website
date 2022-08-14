933feff6129e51f3d6846a3166ea0cb5b31870cc
Wim Onderbeke <wim.onderbeke@zoho.eu>
2022-08-14 12:23:29 +0200

    Undo package upgrade

--- cms/sanity.config.ts
+++ cms/sanity.config.ts
@@ -1,18 +1,18 @@
-import { codeInput } from "@sanity/code-input";
-import { createConfig } from "sanity";
-import { deskTool } from "sanity/desk";
-import { schemaTypes } from "./schemas/schema";
+import { codeInput } from '@sanity/code-input'
+import { createConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { schemaTypes } from './schemas/schema'
 
 export default createConfig({
-  name: "default",
-  title: "My Website",
+	name: 'default',
+	title: 'My Website',
 
-  projectId: "oh3ilzqv",
-  dataset: "production",
+	projectId: 'oh3ilzqv',
+	dataset: 'production',
 
-  plugins: [deskTool(), codeInput()],
+	plugins: [deskTool(), codeInput()],
 
-  schema: {
-    types: schemaTypes,
-  },
-});
+	schema: {
+		types: schemaTypes
+	}
+})
