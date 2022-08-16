4d9f4a554653a9e0084697cddfa619639b8a9973
Wim Onderbeke <wim.onderbeke@zoho.eu>
2022-08-16 21:56:08 +0200

    Add correct generic and type for layout load function

--- site/src/routes/+layout.server.ts
+++ site/src/routes/+layout.server.ts
@@ -1,14 +1,14 @@
 import type { PreviewPost } from '$api/posts';
 import { fetchPosts } from '$api/posts';
 import { fetchWhoamiData } from '$api/whoami';
-import type { PageServerLoad } from './$types';
+import type { LayoutServerLoad } from './$types';
 
 interface LayoutProps {
 	resumeUrl: string;
 	posts: PreviewPost[];
 }
 
-export const load: PageServerLoad<LayoutProps> = async () => {
+export const load: LayoutServerLoad<LayoutProps> = async () => {
 	const whoamiData = await fetchWhoamiData();
 	const posts = await fetchPosts();
 	return {
